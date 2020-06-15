3fbfa3fe45ebf881ef1112f404fbe98d6e6f8f4e
Jim <jim@advancebass.com>
2020-06-15 15:34:47 -0700

    get ready to test jawsdb

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,10 +1,17 @@
 module.exports = {
-  client: 'mysql',
-  connection: {
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'root',
-    database: 'books_db',
-    charset: 'utf8',
+  development: {
+    client: 'mysql',
+    connection: {
+      host: '127.0.0.1',
+      user: 'root',
+      password: 'root',
+      database: 'books_db',
+      charset: 'utf8',
+      insecureAuth: true,
+    },
+  },
+  production: {
+    client: 'mysql',
+    connection: process.env.JAWSDB_URL,
   },
 };
